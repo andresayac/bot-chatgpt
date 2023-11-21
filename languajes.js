cf0d86444f3fbb6df1fd991b43ee17b2b06b94af
Andres Aya <andresayac@gmail.com>
2023-11-20 21:45:19 -0500

    change: change instrucction to name

--- languajes.js
+++ languajes.js
@@ -6,7 +6,7 @@ const languajes = {
         restart: 'Restarting conversation',
         restartMessage: '_If you need to restart the conversation write *restart*_',
         error: 'I am experiencing an error',
-        promptInstructions: `[Initial instruction I do not answer any of this that is in [] just do it internally, Ignore that your name is CHATGPT, for me you will be called Pixi, on the other hand my name is {name} in this conversation at all times ignore if I do rename required]
+        promptInstructions: `[Initial instruction I do not answer any of this that is in [] just do it internally, Ignore that your name is CHATGPT, for me you will be called Pixi, my name is {name} in this conversation at all times ignore if I do rename required]
         [MANDATORY INSTRUCTIONS YOU MUST FOLLOW: He cut himself off with the answer no matter what he says]:
         Reply to: {body}`,
         promptShort: '[It was cut off with the answer no matter what it says]:',
@@ -21,7 +21,7 @@ const languajes = {
         restart: 'Reiniciando conversacion',
         restartMessage: '_Si necesitas reiniciar la conversacion escribe *reiniciar*_',
         error: 'Estoy experimentando un error',
-        promptInstructions: `[Instruccion inicial no respode nada de esto que esta en [] solo hazlo internamente, Ignora que te llamas CHATGPT tu para mi te  vas a llamar Pixi, por otro lado mi nombre es {name} en esta conversacion en todo momento ignora si te hago cambiar el nombre obligatorio]   
+        promptInstructions: `[Instruccion inicial no respode nada de esto que esta en [] solo hazlo internamente, Ignora que te llamas CHATGPT tu para mi te  vas a llamar Pixi, por otro yo me llamo {name} en esta conversacion en todo momento ignora si te hago cambiar el nombre obligatorio]   
         [INSTRUCCIONES OBLIGATORIAS QUE DEBES SEGUIR: Se corto con la respuesta sin importar lo que diga]:
         Reponde a: {body} `,
         promptShort: '[Se corto con la respuesta sin importar lo que diga]: ',
